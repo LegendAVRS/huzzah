cf60e3dfd77a9d1fb43c139c0718ea0df8c482a0
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-09-12 00:07:57 +0700

    chore: metadata stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,22 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Huzzah!</title>
+	<meta property="og:title" content={"Huzzah!"} />
+	<meta property="og:url" content="https://gigi-counter.web.app/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="https://raw.githubusercontent.com/LegendAVRS/huzzah/main/static/huzzah.jpg" />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="theme-color" content="#F5D5AE" />
+	<meta
+		name="description"
+		content={"A huzzah counter for sir Gonathan G, not much to say really"}
+	/>
+	<meta
+		name="og:description"
+		content={"A huzzah counter for sir Gonathan G, not much to say really"}
+	/>
+</svelte:head>
+
 <slot />
