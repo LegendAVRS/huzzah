9edf10d4efcdb3815a07d8da516c53b6437e0486
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-09-12 00:15:16 +0700

    chore: changed to sqaure image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 	<meta property="og:url" content="https://gigi-counter.web.app/" />
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="https://raw.githubusercontent.com/LegendAVRS/huzzah/main/static/huzzah.jpg" />
-	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:card" content="summary" />
 	<meta property="theme-color" content="#F5D5AE" />
 	<meta
 		name="description"
